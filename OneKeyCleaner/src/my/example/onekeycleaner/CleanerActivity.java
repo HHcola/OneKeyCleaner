b6a29b11f1f0411a4813d4abaf2dcd459ad5275b
unknown <hewei05@B000000027408.internal.baidu.com>
2014-03-04 21:45:43 +0800

    create project

---
+++ OneKeyCleaner/src/my/example/onekeycleaner/CleanerActivity.java
@@ -0,0 +1,24 @@
+package my.example.onekeycleaner;
+
+import com.example.onekeycleaner.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class CleanerActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_cleaner);
+	}
+
+//	@Override
+//	public boolean onCreateOptionsMenu(Menu menu) {
+//		// Inflate the menu; this adds items to the action bar if it is present.
+//		getMenuInflater().inflate(R.menu.cleaner, menu);
+//		return true;
+//	}
+
+}
